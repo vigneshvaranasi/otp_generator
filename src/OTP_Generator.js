a1629ed8d0186c66b239b849412093299c59d5fd
vigneshvaranasi <varanasivsv@gmail.com>
2024-01-05 16:54:53 +0530

    finish

--- src/OTP_Generator.js
+++ src/OTP_Generator.js
@@ -5,21 +5,22 @@ const OtpGenerator = () => {
   const [otp, setOtp] = useState('');
 
   const generateOtp = () => {
-    // Generate a random 6-digit OTP
     const newOtp = Math.floor(100000 + Math.random() * 900000).toString();
     setOtp(newOtp);
   };
 
   return (
-    <div className='box'>
-      <h1>OTP Generator</h1>
-      <button className='btn' onClick={generateOtp}>Generate OTP</button>
-      {otp && (
-        <div>
-          <h2>Your OTP:</h2>
-          <p className='otp'>{otp}</p>
-        </div>
-      )}
+    <div className='body'>
+      <div className='box'>
+        <h1>OTP Generator</h1>
+        <button className='btn' onClick={generateOtp}>Generate OTP</button>
+        {otp && (
+          <div>
+            <h2>Your OTP:</h2>
+            <p className='otp'>{otp}</p>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
